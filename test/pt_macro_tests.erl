3dd389954f019d141a0756e6e87e3b85e38bb772
Andrey Teplyashin <platinumthinker@gmail.com>
2015-11-30 22:50:22 +0600

    Fix and split eunit tests

--- test/pt_macro_tests.erl
+++ test/pt_macro_tests.erl
@@ -1,19 +1,20 @@
 %%%-------------------------------------------------------------------
 %%% -*- coding: utf-8 -*-
 %%% @copyright (C) 2015, Eltex, Novosibirsk, Russia
-%%% @doc
-%%%
-%%% @end
 %%%-------------------------------------------------------------------
 -module(pt_macro_tests).
 
 -include_lib("eunit/include/eunit.hrl").
--include("../include/pt_macro.hrl").
+-include("pt_macro.hrl").
 
-pt_macro_test_() ->
-    [
-        ?_test('pt_macro_test_/0' = ?FUNC),
-        ?_test(pt_macro_test_ = ?FUNC_NAME),
-        ?_test("pt_macro_test_/0" = ?FUNC_STRING),
-        ?_test(2 =:= pt_lib_tests:?FUNC_NAME(1))
-    ].
+pt_macro_func_name_with_arity_test() ->
+    ?_test('pt_macro_func_name_with_arity_test/0' = ?FUNC).
+
+pt_macro_func_name_without_arity_test() ->
+    ?_test(pt_macro_func_name_without_arity_test = ?FUNC_NAME).
+
+pt_macro_string_func_name_with_arity_test() ->
+    ?_test("pt_macro_string_func_name_with_arity_test/0" = ?FUNC_STRING).
+
+pt_macro_string_func_name_without_arity_test() ->
+    ?_test(2 =:= pt_lib_tests:?FUNC_NAME(1)).
